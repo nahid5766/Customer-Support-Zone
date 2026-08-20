ecbe906c1e7f07585538b396ae53c46c9a33a015
nahid5766 <nahidurrahman6244@gmail.com>
2026-08-20 16:44:37 +0600

    prop drilling commit

--- src/components/TicketCart/TicketCart.jsx
+++ src/components/TicketCart/TicketCart.jsx
@@ -22,7 +22,9 @@ const TicketCart = ({ customersPromise }) => {
                         <div class="grid grid-cols-1 md:grid-cols-2 gap-5">
 
                             {/*  Card 1  */}
-                            
+                            {
+                                customersData.map(customer => <Cart customer={customer}></Cart>)
+                            }
 
                         </div>
                     </div>
